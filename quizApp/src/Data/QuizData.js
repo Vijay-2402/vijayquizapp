43de9dcbfa9191dc0431a4216a67d7d37c918c2c
Vijay-2402 <115980340+Vijay-2402@users.noreply.github.com>
2023-09-20 18:25:20 +0530

    Add files via upload

---
+++ quizApp/src/Data/QuizData.js
@@ -0,0 +1,53 @@
+export const QuizData = [
+    {
+        question: "Which language runs in a web browser?",
+        options:[ "Java", "C", "Python", "JavaScript"],
+        answer: 4
+    },
+    {
+        question: "Who invented java programming?",
+        options : ["Guido van rossum","james Gosling","Dennis Ritche","Bjourne Stroustrup"],
+        answer: 2
+    },
+    {
+        question: "What does CSS stand for?",
+        options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+        answer: 2
+    },
+    {
+        question:"which component is used to compile,debug and execute java programs?",
+        options:["jdk","jit","jre","jvm"],
+        answer:1
+    },
+    {
+        question:"which one of the following is not a java feature ?",
+        options:["object-oriented","use of pointers","Portable","dynamic and extensible"],
+        answer:2
+    },
+    {
+        question:"Which of these cannot be used for a variable name in java?",
+        options:["identifier and keyword","identifier","Keyword","none"],
+        answer:3
+
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        answer: 1
+    },
+    {
+        question: "What year was JavaScript launched?",
+        options: ["1996", "1995", "1994", "none of the above"],
+        answer:2
+    },
+    {
+        question: "What is the extension of java code files?",
+        options: [".js",".txt",".class",".java"],
+        answer:4
+    },
+    {
+        question: "Which of the following is a type of polmorphism in java?",
+        options: ["Multiple polmorphism","Compile time polymorphism","Multilevel polymorphism","Execution level polmorphism"],
+        answer:2
+    },
+];
